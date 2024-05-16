3b9b2b37251e800bda88007d1ed451d540819dec
vimla kushwaha <vimlakushwaha01@gmail.com>
2024-05-16 22:27:11 +0530

    Initial Commit

---
+++ stepper/stepperApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepperApp.swift
+//  stepper
+//
+//  Created by vimla kushwaha on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct stepperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
